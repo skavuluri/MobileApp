8176c53824a0283a9d90668971d7f02cc4fdd9f6
skavuluri123 <64663610+skavuluri123@users.noreply.github.com>
2020-11-07 11:28:36 +0530

    First react native page

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import WelcomeScreen from './app/assets/Screens/WelcomeScreen';
+
 
 export default function App() {
+  console.log("App executed");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <WelcomeScreen/>
   );
 }
 
